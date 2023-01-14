b6a0a44b63f377f990dd8491504f3a605e190417
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2023-01-14 12:15:25 +0400

    navbar finished

---
+++ components/navbar/navbar.js
@@ -0,0 +1,36 @@
+import styles from './navbar.module.css'
+
+const Navbar = (props) => {
+  const { username } = props
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href='/'>
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+            </button>
+            <div className={styles.navDropDown}>
+              <div>
+                <a className={styles.linkName} href=''>
+                  Sign Out
+                </a>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
